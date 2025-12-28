5a3de3690937c180b7e1e5b19ff48e5e4c2502a8
aalqadhibi <12425@holbertonstudents.com>
2025-12-28 17:25:54 +0000

    some changes

--- main.c
+++ main.c
@@ -155,7 +155,7 @@ int main(int argc, char *argv[], char *envp[])
 			if (WIFEXITED(status))
 				last_status = WEXITSTATUS(status);
 			cmd_count++;
-		}/*   Parent process */
+		}
 	}
 
 	free(line);
